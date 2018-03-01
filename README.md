1df8824548baea6c58d1b4af9e5f9ddcab446597
caokai <caokai@didichuxing.com>
2018-03-01 20:46:14 +0800

    新脚本

---
+++ README.md
@@ -0,0 +1,5 @@
+# 用于初始化环境centos
+
+zsh, oh-my-zsh, 添加跳转插件
+
+vim，vim配置
---
+++ init_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo yum update
+sudo yum install -y zsh
+
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+git clone https://github.com/ckLXHL/cppProject
